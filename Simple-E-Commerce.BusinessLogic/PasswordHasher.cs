730bfa428064d8a724486aa692b18222b2627456
AbdelrahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2025-02-01 04:53:33 +0200

    Fixed Bug were computed hashes never equaled stored ones

--- Simple-E-Commerce.BusinessLogic/PasswordHasher.cs
+++ Simple-E-Commerce.BusinessLogic/PasswordHasher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -12,47 +13,57 @@ namespace Simple_E_Commerce.BusinessLogic
     {
         public static (string HashString, string SaltString) HashPassword(string InputPassword)
         {
-            using(HMACSHA256 HMAC = new HMACSHA256())
+            using (HMACSHA256 HMAC = new HMACSHA256())
             {
+                // Generate a random salt
                 byte[] Salt = new byte[16];
                 RandomNumberGenerator RNG = RandomNumberGenerator.Create();
                 RNG.GetBytes(Salt);
 
+                // Combine the password and salt
                 byte[] PasswordBytes = Encoding.UTF8.GetBytes(InputPassword);
                 byte[] SaltedPassword = new byte[PasswordBytes.Length + Salt.Length];
 
                 Array.Copy(PasswordBytes, 0, SaltedPassword, 0, PasswordBytes.Length);
                 Array.Copy(Salt, 0, SaltedPassword, PasswordBytes.Length, Salt.Length);
 
+                // Initialize HMAC with the salt as the key
+                HMAC.Key = Salt;
+
+                // Compute the hash of the salted password
                 byte[] Hash = HMAC.ComputeHash(SaltedPassword);
 
-                return(Convert.ToBase64String(Hash), Convert.ToBase64String(Salt));
+                // Return the hash and the salt (base64 encoded)
+                return (Convert.ToBase64String(Hash), Convert.ToBase64String(Salt));
             }
         }
 
         public static bool VerifyPassword(string InputPassword, string StoredHash, string StoredSalt)
         {
-            byte[] HashBytes = Convert.FromBase64String(StoredHash);
+            // Decode the stored hash and salt from Base64 to byte arrays
+            byte[] StoredHashBytes = Convert.FromBase64String(StoredHash);
             byte[] SaltBytes = Convert.FromBase64String(StoredSalt);
+
+            // Convert the input password to a byte array
             byte[] InputPasswordBytes = Encoding.UTF8.GetBytes(InputPassword);
 
-            using (HMACSHA256 HMAC = new HMACSHA256())
+            // Combine the input password and salt
+            byte[] SaltedPassword = new byte[InputPasswordBytes.Length + SaltBytes.Length];
+            Array.Copy(InputPasswordBytes, 0, SaltedPassword, 0, InputPasswordBytes.Length);
+            Array.Copy(SaltBytes, 0, SaltedPassword, InputPasswordBytes.Length, SaltBytes.Length);
+
+            // Initialize HMAC with the stored salt as the key
+            using (HMACSHA256 hmac = new HMACSHA256())
             {
-                byte[] SaltedPassword = new byte[HashBytes.Length + SaltBytes.Length];
-                Array.Copy(InputPasswordBytes, 0, SaltedPassword, 0, InputPasswordBytes.Length);
-                Array.Copy(SaltBytes, 0, SaltedPassword, InputPasswordBytes.Length, SaltBytes.Length);
+                hmac.Key = SaltBytes;  // Set the key to the stored salt
 
-                byte[] CompareHash = HMAC.ComputeHash(SaltedPassword);
+                // Compute the hash of the salted password
+                byte[] computedHashBytes = hmac.ComputeHash(SaltedPassword);
 
-                for(int i =  0; i < HashBytes.Length; i++)
-                {
-                    if (StoredHash[i] != CompareHash[i])
-                    {
-                        return false;
-                    }
-                }
-                return true;
+                // Compare the computed hash with the stored hash
+                return StoredHashBytes.SequenceEqual(computedHashBytes);
             }
         }
+
     }
 }
